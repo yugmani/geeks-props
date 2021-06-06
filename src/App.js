c5f4d50fdf6a36b9309a6345f76143f732840a68
yugmani <yugmani@yahoo.com>
2021-06-05 17:52:33 -0700

    accessing a prop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
-export default function App() {
+export default function App(props) {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Hello {props.user}👨!</h1>
+      <p>Start editing to see some magic happen 😊</p>
     </div>
   );
 }
